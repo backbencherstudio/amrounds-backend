f896808eb7fa811d8b557294103a75f10fce08f0
sazzad-git <sazzadurrahman000@gmail.com>
2026-01-14 14:49:11 +0600

    update prisma model

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -329,15 +329,11 @@ export class AuthService {
 
   async register({
     name,
-    first_name,
-    last_name,
     email,
     password,
     type,
   }: {
     name: string;
-    first_name: string;
-    last_name: string;
     email: string;
     password: string;
     type?: string;
@@ -358,8 +354,6 @@ export class AuthService {
 
       const user = await this.userRepository.createUser({
         name: name,
-        first_name: first_name,
-        last_name: last_name,
         email: email,
         password: password,
         type: type,
