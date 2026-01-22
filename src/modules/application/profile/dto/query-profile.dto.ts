280f7b650daacf5465da990df0eb7f35c693a7fc
sazzad-git <sazzadurrahman000@gmail.com>
2026-01-22 16:44:11 +0600

    feat: Implement profile discovery with fuzzy matching and detailed profile retrieval.

--- src/modules/application/profile/dto/query-profile.dto.ts
+++ src/modules/application/profile/dto/query-profile.dto.ts
@@ -7,12 +7,12 @@ export class DiscoverProfileQueryDTO {
   search?: string;
 
   @IsOptional()
-  @Transform((value) => (value ? Number(value) : 1))
+  @Transform(({ value }) => (value ? Number(value) : 1))
   @IsNumber()
   page?: number = 1;
 
   @IsOptional()
-  @Transform((value) => (value ? Number(value) : 10))
+  @Transform(({ value }) => (value ? Number(value) : 10))
   @IsNumber()
   limit?: number = 10;
 }
