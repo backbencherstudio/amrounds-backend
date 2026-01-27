60e09177350cea134bf6290db2e74f96108856ed
najim2004 <najim10112004@gmail.com>
2026-01-27 16:30:07 +0600

    feat: Implement contact form submission with admin email notifications and add a user leaderboard service.

--- src/modules/application/contact/contact.service.ts
+++ src/modules/application/contact/contact.service.ts
@@ -1,13 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import { CreateContactDto } from './dto/create-contact.dto';
 import { PrismaService } from '../../../prisma/prisma.service';
+import { MailService } from 'src/mail/mail.service';
+import { Role } from 'src/common/guard/role/role.enum';
 
 @Injectable()
 export class ContactService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private mailService: MailService,
+  ) {}
 
   async create(createContactDto: CreateContactDto) {
-    const data = {};
+    const data: any = {};
     if (createContactDto.name) {
       data['name'] = createContactDto.name;
     }
@@ -26,12 +31,42 @@ export class ContactService {
     if (createContactDto.message) {
       data['message'] = createContactDto.message;
     }
+    // Default subject
+    data['subject'] = `New Contact Message`;
 
-    await this.prisma.contact.create({
+    const contact = await this.prisma.contact.create({
       data,
     });
 
-    // TODO: send email to admin
+    // Send email to all admins
+    const admins = await this.prisma.user.findMany({
+      where: {
+        roles: {
+          some: {
+            name: Role.ADMIN,
+          },
+        },
+      },
+      select: { email: true },
+    });
+
+    try {
+      for (const admin of admins) {
+        if (admin.email) {
+          await this.mailService.sendContactNotification({
+            adminEmail: admin.email,
+            contact: {
+              name: data.name || `${data.first_name} ${data.last_name}`,
+              email: data.email,
+              subject: data.subject,
+              message: data.message,
+            },
+          });
+        }
+      }
+    } catch (error) {
+      console.log(error);
+    }
 
     return {
       success: true,
