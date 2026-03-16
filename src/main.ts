cd8c35139caf1927e7ea70d0594879dfc2e6bd88
Sazzadur Rahman <sazzadurrahman000@gmail.com>
2026-03-16 09:03:04 +0600

    solve large entity issue

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import helmet from 'helmet';
 import { join } from 'path';
-// import express from 'express';
+import * as express from 'express';
 // internal imports
 import { AppModule } from './app.module';
 import appConfig from './config/app.config';
@@ -19,6 +19,9 @@ async function bootstrap() {
 
   // Handle raw body for webhooks
   // app.use('/payment/stripe/webhook', express.raw({ type: 'application/json' }));
+
+  app.use(express.json({ limit: '50mb' }));
+  app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
   app.setGlobalPrefix('api');
 
