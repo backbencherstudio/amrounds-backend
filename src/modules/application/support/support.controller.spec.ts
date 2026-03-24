265c8e2c3d12bbeb8b2e4f5a2e497988029f58f8
Sazzadur Rahman <sazzadurrahman000@gmail.com>
2026-03-24 17:16:43 +0600

    add admin message

--- src/modules/application/support/support.controller.spec.ts
+++ src/modules/application/support/support.controller.spec.ts
@@ -2,13 +2,21 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { SupportController } from './support.controller';
 import { SupportService } from './support.service';
 
+const mockSupportService = {
+  create: jest.fn().mockReturnValue('This action adds a new support'),
+  findAll: jest.fn().mockReturnValue('This action returns all support'),
+  findOne: jest.fn().mockReturnValue('This action returns a #1 support'),
+  update: jest.fn().mockReturnValue('This action updates a #1 support'),
+  remove: jest.fn().mockReturnValue('This action removes a #1 support'),
+};
+
 describe('SupportController', () => {
   let controller: SupportController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [SupportController],
-      providers: [SupportService],
+      providers: [{ provide: SupportService, useValue: mockSupportService }],
     }).compile();
 
     controller = module.get<SupportController>(SupportController);
