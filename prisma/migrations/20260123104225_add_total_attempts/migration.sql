075eca025cfe06c97625e7b940bd2fd6ca112e46
Sazzadur Rahman <sazzadurrahman000@gmail.com>
2026-04-08 16:45:01 +0600

    completed backend

--- prisma/migrations/20260123104225_add_total_attempts/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "tests" ADD COLUMN     "total_attempts" INTEGER NOT NULL DEFAULT 0;
--- prisma/migrations/20260124051454_update_test_mode/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - The `test_mode` column on the `tests` table would be dropped and recreated. This will lead to data loss if there is data in the column.
-
-*/
--- AlterTable
-ALTER TABLE "tests" DROP COLUMN "test_mode",
-ADD COLUMN     "test_mode" TEXT[];
